9edd6df1f16f65fc1c3b0eb80fd26875a1be000c
matthew <matthewgoodbar@gmail.com>
2022-11-09 15:35:33 -0800

    Implement phase 1

--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :users
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  post '/users', to: 'users#create'
+  patch '/users:id', to: 'users#update'
+  put '/users:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
 end
