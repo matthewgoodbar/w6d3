68028b060a75c91aa9e784662fc9fe88bbfd5feb
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-10 15:27:52 -0800

    Complete phase 4

--- routes_and_controllers/app/controllers/comments_controller.rb
+++ routes_and_controllers/app/controllers/comments_controller.rb
@@ -22,9 +22,23 @@ class CommentsController < ApplicationController
     end
 
     def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            render json: comment
+        else
+            render json: comment.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        comment = Comment.find_by(id: params[:id])
+        if comment
+            comment.destroy
+            render json: comment
+        else
+            render json: 'Comment does not exist', status: 404
+        end
+
     end
 
     private
