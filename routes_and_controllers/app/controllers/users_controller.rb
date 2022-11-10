4bdeb8c80e2da6e707d99219a7b3e4409185b831
matthew <matthewgoodbar@gmail.com>
2022-11-09 17:28:05 -0800

    Finish phase 4

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -8,15 +8,35 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(params.require(:user).permit(:name, :email))
-  # replace the `user_attributes_here` with the actual attribute keys
-       if user.save
+        if user.save
             render json: user
-       else
-        render json: user.errors.full_messages, status: :unprocessable_entity
-       end
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        if user.update(user_params)
+            redirect_to user_url(user.id)
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to users_url
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
   post '/users', to: 'users#create'
-  patch '/users:id', to: 'users#update'
-  put '/users:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 
 end
