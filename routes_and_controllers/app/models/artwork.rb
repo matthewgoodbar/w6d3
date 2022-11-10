9742255167b8aa478b63b753647fb6014b870ef1
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-10 11:08:02 -0800

    Complete Phase 1

--- routes_and_controllers/app/models/artwork.rb
+++ routes_and_controllers/app/models/artwork.rb
@@ -13,4 +13,17 @@ class Artwork < ApplicationRecord
     validates :title, presence: true
     validates :image_url, presence: true, uniqueness: true
     validates :artist_id, uniqueness: {scope: :title}
+
+    belongs_to :artist,
+    foreign_key: :artist_id,
+    class_name: :User
+
+    has_many :shares,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare,
+    dependent: :destroy
+
+    has_many :shared_viewers,
+    through: :shares,
+    source: :viewer
 end
--- routes_and_controllers/app/models/user.rb
+++ routes_and_controllers/app/models/user.rb
@@ -9,4 +9,18 @@
 #
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+
+    has_many :artworks,
+    foreign_key: :artist_id,
+    class_name: :Artwork,
+    dependent: :destroy
+
+    has_many :views,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare,
+    dependent: :destroy
+
+    has_many :shared_artworks,
+    through: :views,
+    source: :artwork
 end
